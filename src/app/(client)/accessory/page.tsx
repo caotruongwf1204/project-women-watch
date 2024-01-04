1af52273a0ba984981816662e93be78880091f8e
caotruongwf1204 <caodinhtruong1204@gmail.com>
2024-01-05 04:02:24 +0700

    up

--- src/app/(client)/accessory/page.tsx
+++ src/app/(client)/accessory/page.tsx
@@ -23,7 +23,7 @@ export default async function Accessory() {
             <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-2">
               {products.map(( product: any ) => (
                 <Link key={product.id} href={`/products/${product.id}`}>
-                  <ProductCart key={product.id} {...product}></ProductCart>
+                  <ProductCart key={product.idProductCart} {...product}></ProductCart>
                 </Link>
               ))}
             </div>
