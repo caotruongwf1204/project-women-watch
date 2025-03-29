7460c9675e760a6976d7b5e559372cc502ccff38
Cao Đình Trường <caodinhtruong1204@gmail.com>
2025-03-29 16:50:32 +0700

    add image configuration to next.config.js for external domain support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
+    images: {
+      domains: ['api-product-watch.vercel.app'],
+      unoptimized: true,
+  },
   }
 
 module.exports = nextConfig
